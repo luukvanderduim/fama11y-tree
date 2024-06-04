07fc8445e081fec117ef99ea652408bc61c269b1
Luuk van der Duim <luukvanderduim@gmail.com>
2024-06-04 16:18:28 +0200

    Add iterative version of `A11yNode::from_accessible_proxy`
    Adds:
    - iterative version of `A11yNode::from_accessible_proxy`
    - Some stats on elapsed time
    - Readme docs

--- README.md
+++ README.md
@@ -1,3 +1,68 @@
 # fama11y-tree
 
 An example on how to build a tree of all objects using [atspi](https://github.com/odilia-app/atspi).
+
+```
+     Root
+     ├── Child1
+     │   ├── Grandchild1
+     │   └── Grandchild2
+     │       ├── Great-Grandchild1
+     │       └── Great-Grandchild2
+     └── Child2
+         ├── Grandchild1
+         └── Grandchild2
+```
+
+## MSRV
+
+### Async fn recursive version
+
+The `A11yNode::from_accessible_proxy_recursive` method currently requires Rust 1.77.2 or higher.
+
+Cargo-msrv reports:
+
+```sh
+error[E0733]: recursion in an `async fn` requires boxing                                                                                                                             │
+│   --> src/main.rs:67:5                                                                                                                                                               │
+│    |                                                                                                                                                                                 │
+│ 67 |     async fn from_accessible_proxy(ap: AccessibleProxy<'_>) -> Result<A11yNode> {                                                                                               │
+│    |     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ recursive `async fn`                                                                            │
+│    |                                                                                                                                                                                 │
+│    = note: a recursive `async fn` must be rewritten to return a boxed `dyn Future`                                                                                                   │
+│    = note: consider using the `async_recursion` crate: https://crates.io/crates/async_recursion
+```
+
+### Async iterative version
+
+The `A11yNode::from_accessible_proxy_iterative` method requires Rust 1.75.0 or higher.
+
+## Performance
+
+| processes | objects | recursive method | iterative method |
+|:----------------------:|:-----------------:|:----------------:|:----------------:|
+| 25  | 241  | 11.87871ms  | 31.792694ms  |
+| 26  | 2900  | 288.716071ms  | 474.438153ms  |
+| 28  | 4960  | 992.864247ms  | 808.82431ms  |
+| 27  | 6948  | 1.447283253s  | 1.077571422s  |
+| 28  | 7509  | 1.515564922s  | 1.08042886s  |
+| 30  | 7813  | 1.589614858s  | 1.119880292s  |
+
+It appears 'recursive' is faster with a small number of accessible applications,
+however 'iterative´ is faster when a larger number of objects is exposed on the bus.
+Note that it does not take that many applications to reach the point where iterative is faster.
+
+## Known issues
+
+### xdg-dbus-proxy
+
+`xdg-dbus-proxy` does not seem to implement all methods on the `Accessible` interface.
+This results in an error if applcations require it:
+
+```sh
+Error: MethodError(OwnedErrorName("org.freedesktop.DBus.Error.UnknownMethod"), Some("Method \"GetRole\" with signature \"\" on interface \"org.a11y.atspi.Accessible\" doesn't exist\n"), Msg { type: Error, serial: 53, sender: UniqueName(":1.106"), reply-serial: 48, body: Signature("s"), fds: [] })
+```
+
+### LibreOffice Calc
+
+LibreOffice Calc succeeds in halting
--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@ use atspi::{
     AccessibilityConnection, Role,
 };
 use display_tree::{AsTree, DisplayTree, Style};
+use futures::future::try_join_all;
+use std::vec;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 const REGISTRY_DEST: &str = "org.a11y.atspi.Registry";
 const REGISTRY_PATH: &str = "/org/a11y/atspi/accessible/root";
-const REGISTRY_INTERFACE: &str = "org.a11y.atspi.Accessible";
+const ACCESSIBLE_INTERFACE: &str = "org.a11y.atspi.Accessible";
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 struct A11yNode {
     role: Role,
     children: Vec<A11yNode>,
@@ -62,38 +64,96 @@ impl A11yNode {
 }
 
 impl A11yNode {
-    async fn from_accessible_proxy(ap: AccessibleProxy<'_>) -> Result<Self> {
-        let role = ap.get_role().await?;
-        let child_objs = ap.get_children().await?;
+    fn count_nodes_iterative(&self) -> usize {
+        let mut count = 1;
+        let mut stack = vec![self];
+
+        while let Some(node) = stack.pop() {
+            count += node.children.len();
+            stack.extend(node.children.iter());
+        }
+
+        count
+    }
+
+    async fn from_accessible_proxy_recursive(ap: AccessibleProxy<'_>) -> Result<A11yNode> {
         let connection = ap.inner().connection();
+        let child_objects = ap.get_children().await?;
+        let role = ap.get_role().await?;
 
-        // Convert `Vec<ObjectRef>` to a `Vec<Future<Output = AccessibleProxy>`.
-        let children = child_objs
-            .iter()
-            .map(|child| child.as_accessible_proxy(connection))
-            .collect::<Vec<_>>();
+        let child_proxies = try_join_all(
+            child_objects
+                .iter()
+                .map(|child| child.as_accessible_proxy(connection)),
+        )
+        .await?;
 
-        // Resolve the futures and filter out the errors.
-        let children = futures::future::join_all(children)
-            .await
-            .into_iter()
-            .filter_map(|child| child.ok())
-            .collect::<Vec<_>>();
-
-        // Convert to a `Vec<Future<Output = Result<A11yNode>>`.
-        let children = children
-            .into_iter()
-            .map(|child| Box::pin(Self::from_accessible_proxy(child)))
-            .collect::<Vec<_>>();
-
-        // Resolve the futures and filter out the errors.
-        let children = futures::future::join_all(children)
-            .await
-            .into_iter()
-            .filter_map(|child| child.ok())
-            .collect::<Vec<_>>();
+        let children = try_join_all(
+            child_proxies
+                .into_iter()
+                .map(|child| Box::pin(A11yNode::from_accessible_proxy_recursive(child))),
+        )
+        .await?;
 
         Ok(A11yNode { role, children })
+    }
+
+    async fn from_accessible_proxy_iterative(ap: AccessibleProxy<'_>) -> Result<A11yNode> {
+        let connection = ap.inner().connection().clone();
+        // Contains the processed `A11yNode`'s.
+        let mut nodes: Vec<A11yNode> = Vec::new();
+
+        // Contains the `AccessibleProxy` yet to be processed.
+        let mut stack: Vec<AccessibleProxy> = vec![ap];
+
+        // If the stack has an `AccessibleProxy`, we take the last.
+        while let Some(ap) = stack.pop() {
+            // 1. Get children for the stack's top `AccessibleProxy`.
+            // 2. Get a `Vec<Role>`s for those children.
+            // 3. Extend the stack with the children `AccessibleProxy`'s.
+            // 4. Create 'children', a `Vec<A11yNode>` from roles.
+            // 5. Create an `A11yNode` from the `AccessibleProxy` and `children`.
+            // 6. Push the `A11yNode` to the `nodes` vector.
+
+            let child_objects = ap.get_children().await?;
+            let mut children_proxies = try_join_all(
+                child_objects
+                    .into_iter()
+                    .map(|child| child.into_accessible_proxy(&connection)),
+            )
+            .await?;
+
+            let roles = try_join_all(children_proxies.iter().map(|child| child.get_role())).await?;
+            stack.append(&mut children_proxies);
+
+            let children = roles
+                .into_iter()
+                .map(|role| A11yNode {
+                    role,
+                    children: Vec::new(),
+                })
+                .collect::<Vec<_>>();
+
+            let role = ap.get_role().await?;
+            nodes.push(A11yNode { role, children });
+        }
+
+        let mut fold_stack: Vec<A11yNode> = Vec::with_capacity(nodes.len());
+
+        while let Some(mut node) = nodes.pop() {
+            if node.children.is_empty() {
+                fold_stack.push(node);
+                continue;
+            }
+
+            // If the node has children, we fold in the children from 'fold_stack'.
+            // There may be more on 'fold_stack' than the node requires.
+            let begin = fold_stack.len().saturating_sub(node.children.len());
+            node.children = fold_stack.drain(begin..).collect();
+            fold_stack.push(node);
+        }
+
+        fold_stack.pop().ok_or("No root node built".into())
     }
 }
 
@@ -101,7 +161,7 @@ async fn get_registry_accessible<'a>(conn: &Connection) -> Result<AccessibleProx
     let registry = AccessibleProxy::builder(conn)
         .destination(REGISTRY_DEST)?
         .path(REGISTRY_PATH)?
-        .interface(REGISTRY_INTERFACE)?
+        .interface(ACCESSIBLE_INTERFACE)?
         .cache_properties(CacheProperties::No)
         .build()
         .await?;
@@ -117,19 +177,38 @@ async fn main() -> Result<()> {
     let conn = a11y.connection();
     let registry = get_registry_accessible(conn).await?;
 
-    let no_children = registry.child_count().await?;
-    println!("Number of accessible applications on the a11y-bus: {no_children}");
-    println!("Construct a tree of accessible objects on the a11y-bus\n");
+    let no_applications = registry.child_count().await?;
+
+    println!("Construct a tree of accessible objects on the a11y-bus (iterative method)\n");
 
     let now = std::time::Instant::now();
-    let tree = A11yNode::from_accessible_proxy(registry).await?;
-    let elapsed = now.elapsed();
-    println!("Elapsed time: {:?}", elapsed);
+    let tree1 = A11yNode::from_accessible_proxy_iterative(registry.clone()).await?;
+    let elapsed_iterative = now.elapsed();
+
+    let node_count = tree1.count_nodes_iterative();
+
+    println!("Construct a tree of accessible objects on the a11y-bus (recursive method)\n");
+
+    let now = std::time::Instant::now();
+    let tree2 = A11yNode::from_accessible_proxy_recursive(registry).await?;
+    let elapsed_recursive = now.elapsed();
+
+    println!("| Applications | Nodes | Recursive (ms)| Iterative (ms)|");
+    println!("|--------------|-------|---------------|---------------|");
+    println!(
+        "| {:<12} | {:<5} | {:<12}  | {:<13} |",
+        no_applications,
+        node_count,
+        elapsed_recursive.as_millis(),
+        elapsed_iterative.as_millis()
+    );
+
+    assert_eq!(tree1, tree2);
+    println!("\nBoth trees are found to be equal\n");
 
     println!("\nPress 'Enter' to print the tree...");
     let _ = std::io::stdin().read_line(&mut String::new());
-
-    println!("{}", AsTree::new(&tree));
+    println!("{}", AsTree::new(&tree1));
 
     Ok(())
 }
